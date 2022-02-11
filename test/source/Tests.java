acb9b2096e48eeb9fafba10ad22b4ce037a1f29b
Mohamed Maaliki <me@auoeke.net>
2022-02-11 00:58:33 +0000

    clean up; bump

--- test/source/Tests.java
+++ test/source/Tests.java
@@ -2,7 +2,7 @@ import java.io.File;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
 
-class Tests {
+class UncheckTest {
     @Test
     void test() {
         GradleRunner.create().withProjectDir(new File("test/project")).withArguments("-s", "clean", "compileJava").forwardOutput().build();
