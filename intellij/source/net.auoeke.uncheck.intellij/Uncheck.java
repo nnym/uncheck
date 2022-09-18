e4cb20ee189153842871b69c29d7c3e9314c55e3
Mohamed Maaliki <me@auoeke.net>
2022-09-18 20:26:03 +0100

    0.3.2: move file checking to `Uncheck::enable` in order to synchronize `UnconditionalExceptionHandler` with `HighlightFilter`

--- intellij/source/net.auoeke.uncheck.intellij/Uncheck.java
+++ intellij/source/net.auoeke.uncheck.intellij/Uncheck.java
@@ -4,16 +4,22 @@ import java.io.File;
 import java.util.jar.JarFile;
 import java.util.stream.Stream;
 import com.intellij.compiler.CompilerConfiguration;
-import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.psi.PsiFile;
 
 public class Uncheck {
-    public static boolean enable(Module module) {
+    public static boolean enable(PsiFile file) {
+        if (file != null && !file.isWritable()) {
+            return true;
+        }
+
+        var module = ModuleUtil.findModuleForFile(file);
         return module != null && Stream.of(CompilerConfiguration.getInstance(module.getProject()).getAnnotationProcessingConfiguration(module).getProcessorPath().split(File.pathSeparator))
             .map(File::new)
             .filter(path -> path.exists() && path.getName().endsWith(".jar"))
             .anyMatch(jar -> {
-                try (var file = new JarFile(jar)) {
-                    return file.getEntry("net.auoeke.uncheck") != null;
+                try (var jarFile = new JarFile(jar)) {
+                    return jarFile.getEntry("net.auoeke.uncheck") != null;
                 }
             });
     }
--- intellij/source/net.auoeke.uncheck.intellij/UnconditionalExceptionHandler.java
+++ intellij/source/net.auoeke.uncheck.intellij/UnconditionalExceptionHandler.java
@@ -1,7 +1,6 @@
 package net.auoeke.uncheck.intellij;
 
 import com.intellij.codeInsight.CustomExceptionHandler;
-import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +8,6 @@ import org.jetbrains.annotations.Nullable;
 
 public class UnconditionalExceptionHandler extends CustomExceptionHandler {
     @Override public boolean isHandled(@Nullable PsiElement element, @NotNull PsiClassType exceptionType, PsiElement topElement) {
-        return element != null && Uncheck.enable(ModuleUtil.findModuleForPsiElement(element));
+        return element != null && Uncheck.enable(element.getContainingFile());
     }
 }
